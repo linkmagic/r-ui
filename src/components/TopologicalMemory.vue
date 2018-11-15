a97cc2cf1605d4aeb4fe9de9e95681a6b5c905d3
Yevhen S <linkmagic@gmail.com>
2018-11-15 21:20:27 +0200

    init commit

---
+++ src/components/TopologicalMemory.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="">
+		TopologicalMemory
+	</div>
+</template>
+
+<script lang="ts">
+
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TopologicalMemory extends Vue {
+	@Prop() private someProp!: string;
+
+}
+
+</script>
+
+<style scoped>
+
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+	<div class="home">
+		<TopologicalMemory someProp="Some value..."/>
+	</div>
 </template>
 
 <script lang="ts">
+
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import TopologicalMemory from '@/components/TopologicalMemory.vue';
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+	components: {
+		TopologicalMemory,
+	},
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+
+}
+
 </script>
