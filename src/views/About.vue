b25e22abc5e5637dd244b455334cd4318ddf5f21
Yevhen S <linkmagic@gmail.com>
2018-11-19 22:01:03 +0200

    remove src TypeScript

--- src/views/About.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="about">
-        <h1>RehaB</h1>
-        <h2>Software for the rehabilitation of cognitive abilities</h2>
-        <h3>Free and Opensource product</h3>
-    </div>
-</template>
